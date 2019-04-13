5c24b89ae3cdfd8297895d7cb97818a4eee018ef
Snapps <30825475+Snappsu@users.noreply.github.com>
2019-04-13 15:18:11 -0700

    added main menu files

---
+++ index.js
@@ -0,0 +1,5 @@
+function start(){
+  popup = document.getElementById("popup-container")
+  popup.style.animationName = "popup";
+  popup.style.animationDuration = "1s";
+  popup.style.animationFillMode = "forwards";
